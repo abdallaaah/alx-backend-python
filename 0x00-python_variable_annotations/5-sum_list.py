c25050ff14fe318ed530aceb2749b66cc812cfed
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-06 19:14:53 -0700

    check task 4 and task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """complex annotaion"""
+from typing import List
 
 
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return sum of floatss"""
     sum: float = 0.0
     for x in input_list:
