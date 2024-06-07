4827222d5515f98a0f4bd0d137608c18242bedda
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-06 19:33:29 -0700

    check return dynamin type

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """learn how to return type or type"""
     return (k, v ** 2)
