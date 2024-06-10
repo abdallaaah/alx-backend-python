3421dd550f4ada338d4b626c885ebdd33d0b679c
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-10 15:41:52 -0700

    check create async with asyncio,create_task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,9 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 """crate async function with ayncio.create_task"""
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay):
     """regular wait"""
