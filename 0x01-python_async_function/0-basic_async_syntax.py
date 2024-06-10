c6b20b7286b984d13425365ee66f5020dfaa4994
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-10 14:06:31 -0700

    check task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# this is first task of async await
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """this the basics of the async wait"""
+
+    x = random.uniform(0, max_delay)
+    await asyncio.sleep(x)
+    return (x)
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+learn asynio- wait and courtouines
