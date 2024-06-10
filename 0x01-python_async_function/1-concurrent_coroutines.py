87cf9aea5614dd2c90380ad991ac0e6476dc24bb
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-10 14:53:10 -0700

    check assync with muiltble

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""execute multible corotuines"""
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """execute mulitible"""
+    arr = []
+    for i in range(n):
+        arr.append(await(wait_random(max_delay)))
+    return arr
