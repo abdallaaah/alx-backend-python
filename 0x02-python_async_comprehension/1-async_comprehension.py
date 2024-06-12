c35a7eb02dbb52f24dc0e95e6ff956eda30cb155
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-11 18:56:07 -0700

    check task one

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Import async_generator from the previous task and then write a coroutine"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """coroutine will collect 10 random numbers"""
+    arr = []
+    async for i in async_generator():
+        arr.append(i)
+    return arr
